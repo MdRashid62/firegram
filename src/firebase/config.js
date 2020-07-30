d94dfa75d18c61fdd620ca01103e7efc85c33018
Muhammad Rashid <55427374+MdRashid62@users.noreply.github.com>
2020-07-30 13:44:54 +0500

    initialize

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAOrTl-RmroiwmQcEMSK_2wLCM-V58M5aI",
+    authDomain: "ninja-firegram-69137.firebaseapp.com",
+    databaseURL: "https://ninja-firegram-69137.firebaseio.com",
+    projectId: "ninja-firegram-69137",
+    storageBucket: "ninja-firegram-69137.appspot.com",
+    messagingSenderId: "907028134896",
+    appId: "1:907028134896:web:c80b67a7297aadd6a97b35"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
